5078809fbcea17335d52dbc263797e5095fcd065
MIchael Setiawan <kevinsenjaya@rollingglory.com>
2022-09-27 08:44:17 +0700

    initialize commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int number1 = 12;
+        int number2 = 2;
+        int perkalian = number1 * number2;
+        System.out.println("hasil perkalian = " + perkalian);
+    }
+    
+}
